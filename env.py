5a8cdab83998debd1ea5cd833a0a85c5ef95c5b8
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-09-06 13:06:58 +0200

    Created new project due to getting error with previous pp4, mentor advised some changes.

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["SECRET_KEY"] = 'django-insecure-0m&5w+1d^8x$hauovlq)gvc!t1e*tkloj4xdc7+77js_8%^ifg'
+os.environ["DEVELOPMENT"] = 'True'
+os.environ["API_KEY"] = '2228671f48644d399e8cc37fb14015f6'

43b22a24a8c4ba9eafea3e55795c86570a841d90
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-09-06 13:18:13 +0200

    added elements to settings.py and created ulrs.py, added base.html, index.html  and cards.html

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Apps
+    'news',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +60,9 @@ ROOT_URLCONF = 'mainapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

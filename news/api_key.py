8a59fd2cb499997bfa00d64ceb225c3094730831
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-09-11 10:47:23 +0200

    updated vews.py and apikey.py previous settings

--- news/api_key.py
+++ news/api_key.py
@@ -3,12 +3,8 @@ import os
 
 NEWS_API_KEY = os.environ.get('API_KEY')
 
-def get_news(country='us', category='general', page=1, page_size=24, query=''):
+def get_news(country='us', category='general', page=1, page_size=24):
     url = (f'https://newsapi.org/v2/top-headlines?country={country}'
            f'&category={category}&page={page}&pageSize={page_size}&apiKey={NEWS_API_KEY}')
-    
-    if query:
-        url = url + f'&q={query}'
-    
     response = requests.get(url)
     return response.json()
--- news/views.py
+++ news/views.py
@@ -3,14 +3,12 @@ from .api_key import get_news
 
 
 # Create your views here.
-
 def index(request):
     country = request.GET.get('country', 'us')
     category = request.GET.get('category', 'general')
     page = int(request.GET.get('page', 1))
-    query = request.GET.get('q', '')
     
-    news = get_news(country=country, category=category, page=page, query=query)
+    news = get_news(country=country, category=category, page=page)
     articles = news.get('articles', [])
 
     return render(request, 'home/cards.html', {

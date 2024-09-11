c2edf8873a7e594afd7edf45e69a6d4ab6d067b6
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-09-11 10:42:38 +0200

    added active elements in nabvbar

--- news/api_key.py
+++ news/api_key.py
@@ -3,8 +3,12 @@ import os
 
 NEWS_API_KEY = os.environ.get('API_KEY')
 
-def get_news(country='us', category='general', page=1, page_size=24):
+def get_news(country='us', category='general', page=1, page_size=24, query=''):
     url = (f'https://newsapi.org/v2/top-headlines?country={country}'
            f'&category={category}&page={page}&pageSize={page_size}&apiKey={NEWS_API_KEY}')
+    
+    if query:
+        url = url + f'&q={query}'
+    
     response = requests.get(url)
     return response.json()
--- news/views.py
+++ news/views.py
@@ -8,8 +8,9 @@ def index(request):
     country = request.GET.get('country', 'us')
     category = request.GET.get('category', 'general')
     page = int(request.GET.get('page', 1))
+    query = request.GET.get('q', '')
     
-    news = get_news(country=country, category=category, page=page)
+    news = get_news(country=country, category=category, page=page, query=query)
     articles = news.get('articles', [])
 
     return render(request, 'home/cards.html', {

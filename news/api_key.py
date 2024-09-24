562017a8c2b3837eabd8a2c9e9fb86d82b6e07a6
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-09-24 15:28:40 +0200

    added active options in categories in navbar

--- news/api_key.py
+++ news/api_key.py
@@ -7,4 +7,8 @@ def get_news(country='us', category='general', page=1, page_size=24):
     url = (f'https://newsapi.org/v2/top-headlines?country={country}'
            f'&category={category}&page={page}&pageSize={page_size}&apiKey={NEWS_API_KEY}')
     response = requests.get(url)
-    return response.json()
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {'articles': []}
